1a8bcdcebd57a79d2c77c8e519a403c3e4c49f8e
ming <mglee.developer@gmail.com>
2020-12-10 18:11:30 +0900

    js: 좌석별 금액 localstorage 저장

--- Movie Seat Booking/main.js
+++ Movie Seat Booking/main.js
@@ -1,19 +1,21 @@
+const movie = document.querySelector('#movie-select');
 const seatContainer = document.querySelector('.seat-container');
+const bookingTitle = document.querySelector('.booking__title');
 const bookingCount = document.querySelector('.count');
-
-let count = 0;  
+const bookingPrice = document.querySelector('.price');
 
 const CLICKED_SEAT = 'clicked';
 
+const MOVIE_LS  = 'selectedMovie'
 const ECONOMY_LS = 'economy';
 const SWEET_LS = 'sweet';
 const DISABLED_LS = 'disabled';
 const REGULAR_LS = 'regular';
 
-const DISABLED_PRICE = 6000;
-const ECONOMY_PRICE = 13000;
-const REGULAR_PRICE = 14000;
-const SWEET_PRICE = 34000;
+const DISABLED_PRICE = 'disabledPrice';//6000;
+const ECONOMY_PRICE = 'economyPrice';//13000;
+const REGULAR_PRICE = 'regularPrice';//14000;
+const SWEET_PRICE = 'sweetPrice';//34000;
 
 function mappingSeat(seat, i, j) {
     if(i === 0) {
@@ -47,16 +49,32 @@ function createSeat() {
     }
 }
 
+function setMovieTitle(movieIndex) {
+    localStorage.setItem(MOVIE_LS, movieIndex);
+}
+
 function selectMovie(e) {
     const target = e.target;
-    const text = target.options[target.selectedIndex].text;
-    const bookingTitle = document.querySelector('.booking__title');
-    bookingTitle.innerText = `${text}`;
+    setMovieTitle(target.selectedIndex);
+    bookingTitle.innerText = `${target.options[target.selectedIndex].text}`;
+}
+
+function setMovieSeat(economyIndex, sweetIndex, disabledIndex, regularIndex) {
+    localStorage.setItem(ECONOMY_LS, JSON.stringify(economyIndex));
+    localStorage.setItem(SWEET_LS, JSON.stringify(sweetIndex));
+    localStorage.setItem(DISABLED_LS, JSON.stringify(disabledIndex));
+    localStorage.setItem(REGULAR_LS, JSON.stringify(regularIndex));
 }
 
 function countAndTotalPrice(economySeats, sweetSeats, disabledSeats, regularSeats) {
     const count = economySeats.length + sweetSeats.length + disabledSeats.length + regularSeats.length;
+    const price = (economySeats.length * localStorage.getItem(ECONOMY_PRICE)) + 
+                    (sweetSeats.length * localStorage.getItem(SWEET_PRICE)) + 
+                    (disabledSeats.length * localStorage.getItem(DISABLED_PRICE)) + 
+                    (regularSeats.length * localStorage.getItem(REGULAR_PRICE));
+                    
     bookingCount.innerText = count;
+    bookingPrice.innerText = price;
 }
 
 function reserveSeat(seats) {
@@ -70,14 +88,23 @@ function reserveSeat(seats) {
     const disabledIndex = [...disabledSeats].map(seat => [...seats].indexOf(seat));
     const regularIndex = [...regularSeats].map(seat => [...seats].indexOf(seat));
     
-    // local storage
-    localStorage.setItem(ECONOMY_LS, JSON.stringify(economyIndex));
-    localStorage.setItem(SWEET_LS, JSON.stringify(sweetIndex));
-    localStorage.setItem(DISABLED_LS, JSON.stringify(disabledIndex));
-    localStorage.setItem(REGULAR_LS, JSON.stringify(regularIndex));
+    //좌석 인덱스 저장
+    setMovieSeat(economyIndex, sweetIndex, disabledIndex, regularIndex);
     
-    // 예약 수 및 총 금액 계산
+    //예약 수 및 총 금액 계산
     countAndTotalPrice(economySeats, sweetSeats, disabledSeats, regularSeats);
+}
+
+function setMoviePrice(selectedSeats) {
+    if(selectedSeats.classList.contains('economy-type')) {
+        localStorage.setItem(ECONOMY_PRICE, 13000);
+    }else if(selectedSeats.classList.contains('sweet-type')) {
+        localStorage.setItem(SWEET_PRICE, 17000);
+    }else if(selectedSeats.classList.contains('disabled-type')) {
+        localStorage.setItem(DISABLED_PRICE, 6000);
+    }else {
+        localStorage.setItem(REGULAR_PRICE, 14000);
+    }
 }
 
 function loadUI() {
@@ -95,17 +122,22 @@ function loadUI() {
                 (disabledSeats.indexOf(index) > -1) || (regularSeats.indexOf(index) > -1)) {
                 seat.classList.add(CLICKED_SEAT);
             }
-
         })
+
+        // 초기 예약 좌석 수, 총 금액
+        countAndTotalPrice(economySeats, sweetSeats, disabledSeats, regularSeats);
     }
 
-    // 초기 예약 좌석 수, 총 금액
-    countAndTotalPrice(economySeats, sweetSeats, disabledSeats, regularSeats);
+    // 선택한 영화 제목
+    const selectedMovieIndex = localStorage.getItem(MOVIE_LS);
+    if(selectedMovieIndex !== null) {
+        movie.selectedIndex = selectedMovieIndex;
+        bookingTitle.innerText = `${movie.options[selectedMovieIndex].text}`;
+    }
 }
 
 function init() {
     // 영화 선택
-    const movie = document.querySelector('#movie-select');
     movie.addEventListener('change', selectMovie);
 
     // 좌석
@@ -116,6 +148,8 @@ function init() {
         const seats = document.querySelectorAll('.seat');
         if(e.target.classList.contains('seat')) {
             e.target.classList.toggle(CLICKED_SEAT);
+            // 좌석별 금액 데이터 저장
+            setMoviePrice(e.target);
             reserveSeat(seats);
         }
     });
