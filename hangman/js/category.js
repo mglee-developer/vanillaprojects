91ed669033991d6545d17fc06f70acea236306a8
ming <mglee.developer@gmail.com>
2020-12-24 14:32:33 +0900

    Update category.js

--- hangman/js/category.js
+++ hangman/js/category.js
@@ -1,14 +1,33 @@
 const category = ['FRUIT', 'ANIMAL', 'CITY'];
+const quiz = [['apple', 'banana', 'grape'],
+                ['dog', 'monky', 'rabbit'],
+                ['seoul', 'busan', 'jeju']];
 
 const categoryName = document.querySelector('.category-name');
+const words = document.querySelector('.words');
 
 function randomCategory() {
     const index = Math.floor(Math.random()*category.length);
     categoryName.innerHTML = `Category - ${category[index]}`;
+
+    return index;
+}
+
+function randomWord() {
+    const returnIndex = randomCategory();
+    const quizIndex = Math.floor(Math.random() * quiz.length);
+    const word = quiz[returnIndex][quizIndex]; 
+    
+    for(let i = 0; i < word.length; i++) {
+        const element = document.createElement('li');
+        element.setAttribute('class', 'word');
+        words.appendChild(element);
+    }
+    
 }
 
 function init() {
-    randomCategory();
+    randomWord();
 }
 
 init();
